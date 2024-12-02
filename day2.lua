cdfb59d1849ccff57f5706a17e0d9b6aa3d56a56
Sebastian Hans <ich@sebastian-hans.de>
2024-12-02 20:30:40 +0100

    solve day 2, part 2

--- day2.lua
+++ day2.lua
@@ -33,11 +33,42 @@ function is_safe(report)
   return true
 end
 
-local safe_count = 0
+local safe_count_1 = 0
 for line in io.lines('input/day2.txt') do
   if is_safe(parse_numbers(line)) then
-    safe_count = safe_count + 1
+    safe_count_1 = safe_count_1 + 1
   end
 end
 
-print(safe_count)
+print(safe_count_1)
+
+function copy_array_removing(t, index_to_remove)
+  local t2 = {}
+  for i = 1, #t do
+    if i ~= index_to_remove then
+      t2[#t2 + 1] = t[i]
+    end
+  end
+  return t2
+end
+
+function is_safe_tolerating_one_bad_level(report)
+  if is_safe(report) then
+    return true
+  end
+  for i = 1, #report do
+    if is_safe(copy_array_removing(report, i)) then
+      return true
+    end
+  end
+  return false
+end
+
+local safe_count_2 = 0
+for line in io.lines('input/day2.txt') do
+  if is_safe_tolerating_one_bad_level(parse_numbers(line)) then
+    safe_count_2 = safe_count_2 + 1
+  end
+end
+
+print(safe_count_2)
