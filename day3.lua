d2652d1eda20f4342adedbf4461b14e7e0f4701a
Sebastian Hans <ich@sebastian-hans.de>
2024-12-03 22:13:10 +0100

    solve day 3, part 2

--- day3.lua
+++ day3.lua
@@ -6,3 +6,27 @@ for line in io.lines('input/day3.txt') do
 end
 
 print(sum)
+
+local lpeg = require('lpeg')
+local p_number = lpeg.C(lpeg.R'09' * lpeg.R'09'^-2)
+local p_mul = lpeg.C(lpeg.P'mul') * lpeg.P'(' * p_number * lpeg.P',' * p_number * lpeg.P')'
+local p_do = lpeg.C(lpeg.P'do') * lpeg.P'()'
+local p_dont = lpeg.C(lpeg.P"don't") * lpeg.P'()'
+local p_instruction = p_do + p_dont + p_mul
+
+local more_accurate_sum = 0
+local is_enabled = true
+for line in io.lines('input/day3.txt') do
+  for i = 1, #line do
+    op, a, b = p_instruction:match(line, i)
+    if op == 'do' then
+      is_enabled = true
+    elseif op == "don't" then
+      is_enabled = false
+    elseif op == 'mul' and is_enabled then
+      more_accurate_sum = more_accurate_sum + a * b
+    end
+  end
+end
+
+print(more_accurate_sum)
