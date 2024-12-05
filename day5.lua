1bda3d0e4468c62fe0fe9444ab8a09146fef574c
Sebastian Hans <ich@sebastian-hans.de>
2024-12-05 21:40:07 +0100

    solve day 5, part 2 (quick and dirty)

--- day5.lua
+++ day5.lua
@@ -22,11 +22,16 @@ function is_valid(update)
   return true
 end
 
-function middle_page_of(update)
+function parse_update(update)
   local pages = {}
   for page in update:gmatch("(%d+)") do
     pages[#pages + 1] = page
   end
+  return pages
+end
+
+function middle_page_of(update)
+  local pages = parse_update(update)
   return pages[(#pages + 1) / 2]
 end
 
@@ -38,3 +43,35 @@ for _, update in ipairs(updates) do
 end
 
 print(sum_of_middle_pages_of_valid_updates)
+
+function rule_comparator(a, b)
+  for _, rule in ipairs(rules) do
+    if rule[1] == a and rule[2] == b then
+      return true
+    end
+  end
+  return false
+end
+
+function render_update(pages)
+  local update = ","
+  for _, page in ipairs(pages) do
+    update = update .. page .. ","
+  end
+  return update
+end
+
+function correct_update(update)
+  local pages = parse_update(update)
+  table.sort(pages, rule_comparator)
+  return render_update(pages)
+end
+
+local sum_of_middle_pages_of_corrected_updates = 0
+for _, update in ipairs(updates) do
+  if not is_valid(update) then
+    sum_of_middle_pages_of_corrected_updates = sum_of_middle_pages_of_corrected_updates + middle_page_of(correct_update(update))
+  end
+end
+
+print(sum_of_middle_pages_of_corrected_updates)
