9503c1d69639ef7fe8eb0ff5338058cda94cf27a
Sebastian Hans <ich@sebastian-hans.de>
2024-12-06 21:05:33 +0100

    solve day 6, part 2
    This is horribly inefficient, but it works.

--- day6.lua
+++ day6.lua
@@ -25,11 +25,11 @@ end
 map.height = y
 
 map.is_marked = function(self, x, y)
-  return map[x .. "," .. y]
+  return self[x .. "," .. y]
 end
 
-guard.is_on = function(self, map)
-  return self.x > 0 and self.y > 0 and self.x <= map.width and self.y <= map.height
+guard.is_on = function(self, cur_map)
+  return self.x > 0 and self.y > 0 and self.x <= cur_map.width and self.y <= cur_map.height
 end
 
 guard.step = function(self, delta)
@@ -56,6 +56,14 @@ guard.move = function(self, map)
   end
 end
 
+function shallow_clone(o)
+  local o2 = {}
+  for k, v in pairs(o) do
+    o2[k] = v
+  end
+  return o2
+end
+
 local trail = { mark = mark }
 trail.length = function(self)
   local l = 0
@@ -67,9 +75,56 @@ trail.length = function(self)
   return l
 end
 
-while guard:is_on(map) do
-  trail:mark(guard.x, guard.y)
-  guard:move(map)
+local g = shallow_clone(guard)
+while g:is_on(map) do
+  trail:mark(g.x, g.y)
+  g:move(map)
 end
 
 print(trail:length())
+
+function mark_detecting_loop(self, x, y, d)
+  local key = x .. "," .. y
+  if not self[key] then
+    self[key] = { d = true }
+  elseif self[key][d] then
+    return "loop"
+  else
+    self[key][d] = true
+  end
+end
+
+function map_with_obstacle_at(position)
+  local m = {}
+  for k, v in pairs(map) do
+    m[k] = v
+  end
+  m[position] = true
+  return m
+end
+
+function does_loop(guard, my_map)
+  local trail = { mark = mark_detecting_loop }
+  while guard:is_on(my_map) do
+    if trail:mark(guard.x, guard.y, guard.direction) == "loop" then
+      return true
+    end
+    guard:move(my_map)
+  end
+  return false
+end
+
+local num_looping_obstacle_positions = 0
+local n = 1
+for pos, is_mark in pairs(trail) do
+  if is_mark == true and pos ~= guard.x .. "," .. guard.y then
+    n = n + 1
+    local g = shallow_clone(guard)
+    local m = map_with_obstacle_at(pos)
+    if does_loop(g, m) then
+      num_looping_obstacle_positions = num_looping_obstacle_positions + 1
+    end
+  end
+end
+
+print(num_looping_obstacle_positions)
