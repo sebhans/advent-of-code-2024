88c65572c34284d6355420f87b9e20cb2473f8fb
Sebastian Hans <ich@sebastian-hans.de>
2024-12-09 21:32:19 +0100

    solve day 9, part 2
    Very inefficient, but what the hell

--- day9.lua
+++ day9.lua
@@ -46,3 +46,59 @@ local function checksum_of(disk)
 end
 
 print(checksum_of(disk))
+
+local function defrag_whole_files(disk)
+  local function find_file(id)
+    local start = 0
+    for i = 0, #disk do
+      if start == 0 and disk[i] == id then
+        start = i
+      elseif start ~= 0 and disk[i] ~= id then
+        return start, i - start
+      end
+    end
+    return start, #disk + 1 - start
+  end
+
+  local function find_space(min_length, max_i)
+    local start, length = 0, 0
+    for i = 1, max_i do
+      if start == 0 and disk[i] == '.' then
+        start = i
+        length = 1
+        if length >= min_length then
+          return start
+        end
+      elseif start ~= 0 and disk[i] == '.' then
+        length = length + 1
+        if length >= min_length then
+          return start
+        end
+      else
+        start = 0
+      end
+    end
+    return nil
+  end
+
+  local function move(id, start, length, space)
+    for i = start, start + length - 1 do
+      disk[i] = '.'
+    end
+    for i = space, space + length - 1 do
+      disk[i] = id
+    end
+  end
+
+  for id = disk[#disk], 0, -1 do
+    local start, length = find_file(id)
+    local space = find_space(length, start)
+    if space then
+      move(id, start, length, space)
+    end
+  end
+end
+
+local disk2 = read_input()
+defrag_whole_files(disk2)
+print(checksum_of(disk2))
