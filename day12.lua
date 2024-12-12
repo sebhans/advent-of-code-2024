f3ce9c9687b41ed9a6b33879f126bb371ae08c12
Sebastian Hans <ich@sebastian-hans.de>
2024-12-12 21:46:20 +0100

    solve day 12, part 2

--- day12.lua
+++ day12.lua
@@ -71,3 +71,72 @@ local function total_fence_price_by_perimeter(regions)
 end
 
 print(total_fence_price_by_perimeter(regions))
+
+local function bounds(region)
+  local top, left, bottom, right = 1000000, 1000000, 0, 0
+  for key in region:elements() do
+    local coord = m.coord(key)
+    if coord.y < top then top = coord.y end
+    if coord.x < left then left = coord.x end
+    if coord.y > bottom then bottom = coord.y end
+    if coord.x > right then right = coord.x end
+  end
+  return top, left, bottom, right
+end
+
+local function sides(region)
+  local top, left, bottom, right = bounds(region)
+  local sides = 0
+
+  for y = top, bottom do
+    local s = 0
+    local left_above_open, left_below_open = false, false
+    for x = left, right do
+      if region:contains(m.coord(x, y):key()) then
+        local above_open = not region:contains(m.coord(x, y):up():key())
+        if not left_above_open and above_open then s = s + 1 end
+        left_above_open = above_open
+
+        local below_open = not region:contains(m.coord(x, y):down():key())
+        if not left_below_open and below_open then s = s + 1 end
+        left_below_open = below_open
+      else
+        left_above_open, left_below_open = false, false
+      end
+    end
+    sides = sides + s
+  end
+
+  for x = left, right do
+    local s = 0
+    local above_left_open, above_right_open = false, false
+    for y = top, bottom do
+      if region:contains(m.coord(x, y):key()) then
+        local left_open = not region:contains(m.coord(x, y):left():key())
+        if not above_left_open and left_open then s = s + 1 end
+        above_left_open = left_open
+
+        local right_open = not region:contains(m.coord(x, y):right():key())
+        if not above_right_open and right_open then s = s + 1 end
+        above_right_open = right_open
+      else
+        above_left_open, above_right_open = false, false
+      end
+    end
+    sides = sides + s
+  end
+
+  return sides
+end
+
+local function total_fence_price_by_sides(regions)
+  local total_fence_price = 0
+  for region in regions:elements() do
+    local size = region:size()
+    local sides = sides(region)
+    total_fence_price = total_fence_price + size * sides
+  end
+  return total_fence_price
+end
+
+print(total_fence_price_by_sides(regions))
