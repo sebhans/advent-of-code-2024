1efbb1515411198752b8facb932e381da79c91eb
Sebastian Hans <ich@sebastian-hans.de>
2024-12-12 17:18:14 +0100

    solve day 13, part 2

--- day13.lua
+++ day13.lua
@@ -9,43 +9,60 @@ local machine = lpeg.P {
   number = lpeg.C(lpeg.R'09'^1),
 }
 
-local function maximize_b(ax, ay, bx, by, x, y)
-  for nb = math.min(x // bx, y // by, 100), 0, -1 do
-    if (x - nb * bx) % ax == 0 and (y - nb * by) % ay == 0 then
-      local na = (x - nb * bx) // ax
-      if na <= 100 and na * ay + nb * by == y + 0 then
-        return na, nb
-      end
-    end
-  end
-end
-
 local function price(na, nb)
   return na * 3 + nb
 end
 
+local function is_solved(na, ax, ay, nb, bx, by, x, y)
+  return na >= 0 and nb >= 0 and na * ax + nb * bx == x and na * ay + nb * by == y
+end
+
 local function solve(ax, ay, bx, by, x, y)
-  local na1, nb1 = maximize_b(ax, ay, bx, by, x, y)
-  local nb2, na2 = maximize_b(bx, by, ax, ay, x, y)
-  if na1 then
-    local p1 = price(na1, nb1)
-    if na2 then
-      local p2 = price(na2, nb2)
-      return math.min(p1, p2)
-    else
-      return p1
-    end
-  elseif na2 then
-    return price(na2, nb2)
+  local na1 = (x - bx * y / by) / (ax - bx * ay / by)
+  local nb1 = (y - ay * na1) / by
+  local nb2 = (y - ay * x / ax) / (by - ay * bx / ax)
+  local na2 = (x - bx * nb2) / ax
+  na1 = math.floor(na1)
+  nb1 = math.floor(nb1)
+  na2 = math.floor(na2)
+  nb2 = math.floor(nb2)
+  if is_solved(na1, ax, ay, nb1, bx, by, x, y) then
+    return na1, nb1
+  elseif is_solved(na1+1, ax, ay, nb1, bx, by, x, y) then
+    return na1+1, nb1
+  elseif is_solved(na1, ax, ay, nb1+1, bx, by, x, y) then
+    return na1, nb1+1
+  elseif is_solved(na1+1, ax, ay, nb1+1, bx, by, x, y) then
+    return na1+1, nb1+1
+  elseif is_solved(na2, ax, ay, nb2, bx, by, x, y) then
+    return na2, nb2
+  elseif is_solved(na2+1, ax, ay, nb2, bx, by, x, y) then
+    return na2+1, nb2
+  elseif is_solved(na2, ax, ay, nb2+1, bx, by, x, y) then
+    return na2, nb2+1
+  elseif is_solved(na2+1, ax, ay, nb2+1, bx, by, x, y) then
+    return na2+1, nb2+1
   end
 end
 
 local total_price = 0
 i.read_paragraphs('input/day13.txt', function(p)
-  local price = solve(machine:match(p))
-  if price then
-    total_price = total_price + price
+  local ax, ay, bx, by, x, y = machine:match(p)
+  local na, nb = solve(ax + 0, ay + 0, bx + 0, by + 0, x + 0, y + 0)
+  if na then
+    total_price = total_price + price(na, nb)
   end
 end)
-
 print(total_price)
+
+local corrected_total_price = 0
+i.read_paragraphs('input/day13.txt', function(p)
+  local ax, ay, bx, by, x, y = machine:match(p)
+  x = x + 10000000000000
+  y = y + 10000000000000
+  local na, nb = solve(ax + 0, ay + 0, bx + 0, by + 0, x, y)
+  if na then
+    corrected_total_price = corrected_total_price + price(na, nb)
+  end
+end)
+print(corrected_total_price)
