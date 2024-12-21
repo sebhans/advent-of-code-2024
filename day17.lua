7f495f916fd29b4b3b2ba7ffd16419dacd33fe81
Sebastian Hans <ich@sebastian-hans.de>
2024-12-21 22:12:30 +0100

    solve day 17, part 2

--- day17.lua
+++ day17.lua
@@ -1,3 +1,4 @@
+local coll = require('collection_utils.lua')
 local lpeg = require('lpeg')
 
 local debugger = lpeg.P {
@@ -75,3 +76,54 @@ local function run(a, b, c, ...)
 end
 
 print(run(debugger:match(io.open("input/day17.txt"):read("a"))))
+
+local function parse_program(_, _, _, ...)
+  local program = {}
+  for i = 1, select('#', ...) do
+    program[#program + 1] = select(i, ...) + 0
+  end
+  return program
+end
+
+local program = parse_program(debugger:match(io.open("input/day17.txt"):read("a")))
+
+local function does_match(a, program, expected_output)
+  local ip, b, c = 0, 0, 0
+  local out = nil
+  while ip < #program do
+    ip, a, b, c, out = step(program, ip, a, b, c)
+    if out then
+      if out ~= expected_output[1] then
+        return false
+      end
+      table.remove(expected_output, 1)
+    end
+  end
+  return #expected_output == 0
+end
+
+local function shallow_copy_tail(a, n)
+  local copy = {}
+  for i = #a - n + 1, #a do
+    copy[#copy+1] = a[i]
+  end
+  return copy
+end
+
+local function find_next_bits(a, program, num_outputs)
+  for i = 0, 7 do
+    local eo = shallow_copy_tail(program, num_outputs)
+    if does_match(a * 8 + i, program, eo) then
+      if num_outputs == #program then
+        return a * 8 + i
+      end
+      local complete_a = find_next_bits(a * 8 + i, program, num_outputs + 1)
+      if complete_a then
+        return complete_a
+      end
+    end
+  end
+  return nil
+end
+
+print(find_next_bits(0, program, 1))
