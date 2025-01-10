e6bf569c19d7978509ddbe5cef062970bdafe98d
Sebastian Hans <ich@sebastian-hans.de>
2025-01-10 20:40:31 +0100

    solve day 20, part 2

--- day20.lua
+++ day20.lua
@@ -31,21 +31,34 @@ until map:is_goal(pos)
 trace:put(pos, n)
 path[#path + 1] = pos
 
-local function reachable_with_cheat(p)
-  local l = p:left()
-  local r = p:right()
-  return ipairs({ l:left(), l:up(), l:down(), p:up():up(), p:down():down(), r:right(), r:up(), r:down() })
-end
-
-local time_saving_cheats = 0
-local desired_min_saving = 100
-for _, p in ipairs(path) do
-  local t1 = trace:at(p)
-  for _, c in reachable_with_cheat(p) do
-    local t2 = trace:at(c)
-    if type(t2) == "number" and t2 > t1 + 1 + desired_min_saving then
-      time_saving_cheats = time_saving_cheats + 1
+local function reachable_with_cheat(p, max_cheat_time)
+  local dx, dy = -1, -max_cheat_time
+  return function()
+    dx = dx + 1
+    if math.abs(dx) + math.abs(dy) > max_cheat_time then
+      dy = dy + 1
+      if dy > max_cheat_time then
+        return nil
+      end
+      dx = -(max_cheat_time - math.abs(dy))
     end
+    return p:translate(dx, dy), math.abs(dx) + math.abs(dy)
   end
 end
-print(time_saving_cheats)
+
+local function cheat(max_cheat_time, desired_min_saving)
+  local time_saving_cheats = 0
+  for _, p in ipairs(path) do
+    local t1 = trace:at(p)
+    for c, ct in reachable_with_cheat(p, max_cheat_time) do
+      local t2 = trace:at(c)
+      if type(t2) == "number" and t2 >= t1 + ct + desired_min_saving then
+        time_saving_cheats = time_saving_cheats + 1
+      end
+    end
+  end
+  print(time_saving_cheats)
+end
+
+cheat(2, 100)
+cheat(20, 100)
